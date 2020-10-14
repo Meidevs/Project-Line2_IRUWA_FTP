5f83395394de5111794e28affcfcf81bb952c456
Kangseohyun <kshyeon123@gmail.com>
2020-10-14 14:34:32 +0900

    Updated!

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -5,21 +5,6 @@ router.get('/', (req, res) => {
     res.render('auth');
 });
 
-router.get('/confirm', async (req, res) => {
-    try {
-        console.log('hi')
-        console.log(req.body)
-        var response = {
-            user : {
-                status : 0
-            }
-        }
-        res.status(200).send('email.confirm')
-    } catch (err) {
-        console.log(err)
-    }
-});
-
 router.get('/dashboard', (req, res) => {
     res.render('dashboard')
 });
