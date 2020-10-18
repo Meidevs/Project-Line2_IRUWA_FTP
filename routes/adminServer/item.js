ae6fa33bca97fa489e7c80481ccfbed1c76aea6d
Kangseohyun <kshyeon123@gmail.com>
2020-10-18 18:22:08 +0900

    Link url Changed!

--- routes/adminServer/item.js
+++ routes/adminServer/item.js
@@ -3,7 +3,7 @@ const { route } = require('./auth');
 var router = express.Router();
 
 router.get('/listing', (req, res) => {
-    res.status(200).render('adlistionglist');
+    res.status(200).render('adlistinglist');
 })
 
 module.exports = router;
