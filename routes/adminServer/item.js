ae16c739e4b401d3782e4a7c07e470c70d00f7d5
Kangseohyun <kshyeon123@gmail.com>
2020-10-18 18:19:38 +0900

    export failed

--- routes/adminServer/item.js
+++ routes/adminServer/item.js
@@ -1,6 +1,9 @@
 var express = require('express'); 
+const { route } = require('./auth');
 var router = express.Router();
 
 router.get('/listing', (req, res) => {
     res.status(200).render('adlistionglist');
 })
+
+module.exports = router;
