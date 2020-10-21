42e148af1e9b41ca30de037c821db3f7a4e2764d
Kangseohyun <kshyeon123@gmail.com>
2020-10-21 17:47:51 +0900

    Schedule Activated Every Minutes

--- routes/appServer/schedule.js
+++ routes/appServer/schedule.js
@@ -1,7 +1,7 @@
 var cron = require('node-cron');
 var userModel = require('../../public/javascripts/components/userModel');
 var itemModel = require('../../public/javascripts/components/itemModel');
-cron.schedule("* * * * 10", async () => {
+cron.schedule("1 * * * *", async () => {
     try {
         console.log('Schedule')
         var userCouponCnt = await userModel.GET_RECOMMENDATIONS();
