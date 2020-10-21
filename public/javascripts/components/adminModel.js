82c79a72d47e8c50cbdfe12fba338303a08b59d0
Kangseohyun <kshyeon123@gmail.com>
2020-10-21 22:26:15 +0900

    Updated!

--- public/javascripts/components/adminModel.js
+++ public/javascripts/components/adminModel.js
@@ -151,7 +151,7 @@ class Admin {
         return new Promise (
             async (resolve, reject) => {
                 try {
-                    var sql = 'SELECT coupon_uri FROM tb_admin_coupons WHERE used = "N" ORDER BY admin_coupon_seq ASC limit ?';
+                    var sql = 'SELECT * FROM tb_admin_coupons WHERE used = "N" ORDER BY admin_coupon_seq ASC limit ?';
                     var resReturn = await myConnection.query(sql, [data]);
                     resolve(resReturn);
                 } catch (err) {
