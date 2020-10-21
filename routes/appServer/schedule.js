44d4f566adf5c0273e2fe2b9bf61532ca0e53e6b
Kangseohyun <kshyeon123@gmail.com>
2020-10-21 17:58:32 +0900

    Updated!

--- routes/appServer/schedule.js
+++ routes/appServer/schedule.js
@@ -1,12 +1,12 @@
 var cron = require('node-cron');
 var userModel = require('../../public/javascripts/components/userModel');
 var itemModel = require('../../public/javascripts/components/itemModel');
-cron.schedule("1 * * * *", async () => {
+cron.schedule("* * * * *", async () => {
     try {
         console.log('Schedule')
         var userCouponCnt = await userModel.GET_RECOMMENDATIONS();
         console.log(userCouponCnt);
-        var userRecommendationCnt = await userModel.GET_USER_COUPON();
+        // var userRecommendationCnt = await userModel.GET_USER_COUPON();
 
     } catch (err) {
 
