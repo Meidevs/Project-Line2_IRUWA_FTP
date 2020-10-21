675d1ec75e3bc1e95f02dd6e7cfb7e0877c4b474
Kangseohyun <kshyeon123@gmail.com>
2020-10-21 17:45:26 +0900

    Schedule Process ongoing

--- routes/appServer/schedule.js
+++ routes/appServer/schedule.js
@@ -1,9 +1,11 @@
 var cron = require('node-cron');
 var userModel = require('../../public/javascripts/components/userModel');
 var itemModel = require('../../public/javascripts/components/itemModel');
-cron.schedule("00 00 * * *", async () => {
+cron.schedule("* * * * 10", async () => {
     try {
+        console.log('Schedule')
         var userCouponCnt = await userModel.GET_RECOMMENDATIONS();
+        console.log(userCouponCnt);
         var userRecommendationCnt = await userModel.GET_USER_COUPON();
 
     } catch (err) {
