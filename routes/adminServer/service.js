bed4916c750177612c8ec1c7ca892c8ed68befe8
Mid2015M32 <mid2015m32@Mid2015.local>
2020-10-28 03:51:58 +0900

    Parameter Failed

--- routes/adminServer/service.js
+++ routes/adminServer/service.js
@@ -58,7 +58,7 @@ router.post('/setbannedkeyword', async (req, res) => {
             flags : 1,
             message : '금지어 등록에 실패하였습니다.'
         }
-        FromData.title = req.body.keyword;
+        FromData.keyword = req.body.keyword;
         var setBannedResult = await adminModel.setBannedKeyword(FromData);
         if(setBannedResult) {
             resReturn = {
