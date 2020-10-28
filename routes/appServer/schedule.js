cc99c7ad8ef83d36b59404904b521c5f239c3d9a
Mid2015M32 <mid2015m32@Mid2015.local>
2020-10-29 03:17:25 +0900

    Update

--- routes/appServer/schedule.js
+++ routes/appServer/schedule.js
@@ -6,19 +6,15 @@ cron.schedule("* * * * *", async () => {
         console.log('Run Every Minutes')
         var cReceiverList = new Array();
         var userCouponCnt = await userModel.GET_RECOMMENDATIONS();
-        console.log('userCouponCnt', userCouponCnt)
         userCouponCnt.map((data) => {
             if (data.cnt >= 5) {
                 cReceiverList.push(data);
             }
         });
         var cReceiverCnt = cReceiverList.length;
-        console.log('cReceiverCnt', cReceiverCnt)
-        console.log('cReceiverList', cReceiverList)
 
         var cList = await adminModel.getUnuseCoupon(cReceiverCnt);
         var resArray = new Array();
-        console.log('cList', cList)
         if (cReceiverCnt != cList.length) {
             for (var i = 0; i < cList.length; i++) {
                 resArray.push({ user_email: cReceiverList[i].recommendation, coupon: cList[i].coupon_uri, due_date : cList[i].due_date })
@@ -42,7 +38,6 @@ cron.schedule("* * * * *", async () => {
         cList.map((data) => {
             cArray.push(data.admin_coupon_seq);
         });
-        console.log('cArray', cArray)
         if (cList.length > 0) {
             await adminModel.updateUnuseCoupon(cArray);
         }
