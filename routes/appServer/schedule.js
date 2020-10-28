aa8381a940d08ec4efd1946eee8c61fcb31a5fd1
Mid2015M32 <mid2015m32@Mid2015.local>
2020-10-29 02:31:33 +0900

    Schedule

--- routes/appServer/schedule.js
+++ routes/appServer/schedule.js
@@ -12,9 +12,12 @@ cron.schedule("* * * * *", async () => {
             }
         });
         var cReceiverCnt = cReceiverList.length;
+        console.log('cReceiverCnt', cReceiverCnt)
+        console.log('cReceiverList', cReceiverList)
+
         var cList = await adminModel.getUnuseCoupon(cReceiverCnt);
         var resArray = new Array();
-
+        console.log('cList', cList)
         if (cReceiverCnt != cList.length) {
             for (var i = 0; i < cList.length; i++) {
                 resArray.push({ user_email: cReceiverList[i].recommendation, coupon: cList[i].coupon_uri, due_date : cList[i].due_date })
@@ -38,7 +41,7 @@ cron.schedule("* * * * *", async () => {
         cList.map((data) => {
             cArray.push(data.admin_coupon_seq);
         });
-
+        console.log('cArray', cArray)
         if (cList.length > 0) {
             await adminModel.updateUnuseCoupon(cArray);
         }
