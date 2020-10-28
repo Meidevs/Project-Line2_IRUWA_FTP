e875aaee748822dbd0ec621bb8aac78486744b1f
Mid2015M32 <mid2015m32@Mid2015.local>
2020-10-28 23:45:29 +0900

    Unreconized Connect Block

--- routes/adminServer/auth.js
+++ routes/adminServer/auth.js
@@ -31,7 +31,7 @@ router.post('/login', async (req, res) => {
         if (adminExitence == 1) {
             var USER_INFO = await adminModel.LOGIN_ADMIN(FromData);
             if (USER_INFO.length > 0) {
-                req.session.user.user_id = FromData.user_id;
+                req.session.user.user_id = USER_INFO[0].user_id;
                 resReturn = {
                     flags: 0,
                     message: '로그인 되었습니다.'
