cc27f3b25892f05f1ec5d12f5bb0731cdb2f986f
Mid2015M32 <mid2015m32@Mid2015.local>
2020-10-29 02:03:42 +0900

    Update

--- routes/appServer/item.js
+++ routes/appServer/item.js
@@ -336,7 +336,7 @@ router.post('/list/detail', async (req, res) => {
         var CMP_PHONE_LIST = await itemModel.GET_PHONE_LIST(FromData);
         var PROFILE = await userModel.GET_USER_PROFILE(CMP_INFOs.user_seq);
         console.log(PROFILE)
-        if (PROFILE.lenggth > 0) {
+        if (PROFILE.length > 0) {
             CMP_INFOs.profile_uri = PROFILE[0].uri;
         }
 
