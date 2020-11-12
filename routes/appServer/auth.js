a7ba028cf23e62a4a22f27dfe805feae024c03ac
Kangseohyun <kshyeon123@gmail.com>
2020-11-12 16:25:06 +0900

    Password Regenerate Fixing

--- routes/appServer/auth.js
+++ routes/appServer/auth.js
@@ -47,13 +47,14 @@ router.post('/login', async (req, res) => {
             flags: 1,
             message: '아이디를 확인해 주세요.'
         }
+        console.log("Login FromData");
         // !! Distinguish General & Incorporated User Based On Status Value Which Sent From Browser. (0 General User, 1 Incorporated User).
         // Notice! Datas From Browser about General User & Incorporated User are totally Different. 
         // Variables of Data ,user_id for General User and cmp_id for Incorp User, are Different.
         //Encrypt User & Incorporated Password For Security. Hash Password Created Using User_id and User_pw.
         var hashingPassword = await functions.PasswordEncryption(FromData.user_id, FromData.user_pw);
         FromData.user_pw = hashingPassword;
-
+        console.log("hashingPassword", hashingPassword)
         // CHECK_USER_EXISTENCE or CHECK_CMP_EXISTENCE is Checking whether User already Exist or Not. 
         // USER_EXISTENCE == 0 Means that user_id(cmp_id) Which you sent to Server is out of Database.
         // Contrarily, USER_EXISTENCE == 1 Means that user_id is in the Database.
